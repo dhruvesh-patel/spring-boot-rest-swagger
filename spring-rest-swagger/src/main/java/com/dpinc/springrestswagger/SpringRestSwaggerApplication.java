1e1eb159493878ad3d397fdca7c2f26392cca11d
Dhruvesh Patel <dmpatel.1412@gmail.com>
2020-08-28 22:12:29 +0100

    Added spring boot project for swagger integration

---
+++ spring-rest-swagger/src/main/java/com/dpinc/springrestswagger/SpringRestSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.dpinc.springrestswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSwaggerApplication.class, args);
+	}
+
+}
---
+++ spring-rest-swagger/src/test/java/com/dpinc/springrestswagger/SpringRestSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpinc.springrestswagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
